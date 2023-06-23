36b0ae4d7d086cf2c73345b285b46ec0c523ef5f
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-06-23 13:40:22 -0300

    add HeroesModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
+// imports js
+
+//imports Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//imports Module
 import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
+//imports App
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
-    CounterModule
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
