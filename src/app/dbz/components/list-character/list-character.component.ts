a32a2b436bb344989846eb0acf1d9cbe2f8addff
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-06-23 18:32:11 -0300

    rename list-component to list-character-component

---
+++ src/app/dbz/components/list-character/list-character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-list-character',
+  templateUrl: './list-character.component.html',
+  styleUrls: ['./list-character.component.css']
+})
+export class ListCharacterComponent {
+
+}
