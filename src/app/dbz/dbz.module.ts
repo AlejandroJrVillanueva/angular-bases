37f43c66062ea0a5696a25d22bcc1e67e0955a1a
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-06-24 14:33:21 -0300

    add FormsModule

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { MainPageComponent } from './pages/main.page.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
@@ -12,7 +13,8 @@ import { ListCharacterComponent } from './components/list-character/list-charact
     ListCharacterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
    exports: [
     MainPageComponent
