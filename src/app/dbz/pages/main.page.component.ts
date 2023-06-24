3232894ac32e9646af6decc5c2b33f1d3d29653b
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-06-24 14:38:45 -0300

    emit events to parent for character(add & list)

--- src/app/dbz/pages/main.page.component.ts
+++ src/app/dbz/pages/main.page.component.ts
@@ -7,12 +7,25 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent{
+
   public characters: Character[] = [{
     name: 'Krillin',
     power:1000
   },{
     name: 'Goku',
     power: 9500
+  },{
+    name: 'Vegeta',
+    power: 7500
   }];
 
+  onNewCharacter(character:Character): void{
+    console.log({character});
+    this.characters.push(character);
+  }
+
+  onDeleteCharacter(index:number): void{
+    console.log({index});
+    this.characters.splice(index,1);
+  }
 }
