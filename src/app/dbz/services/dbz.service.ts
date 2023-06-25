e5e3303ba8716079404b211ebe0a5d209b69fcaa
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-06-25 16:25:54 -0300

    add private dbzService in MainPage

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -22,7 +22,7 @@ export class DbzService {
     power: 7500
   }];
 
-  onNewCharacter(character:Character): void{
+  addCharacter(character:Character): void{
     const newCharacter:Character = {id:uuid(), ...character};
     // toma todas estas propiedades y exparselas en este nuevo objeto
     this.characters.push(newCharacter);
